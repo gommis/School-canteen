d2226e202032b2cdfa7844762950bd551fe76f85
gommis <47540262+gommis@users.noreply.github.com>
2019-05-27 11:48:58 +1200

    Add files via upload

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 School assessment for DTS, due in week 6
 
 Possible versions:
+
 Ver:1.0
+getting website up and running from the leftover comic book store
 
 Ver:1.1
+Trying to make the website look nice, adding pictures, changing colours and changing the cards.
 
 Ver:1.2
+
 
 ver:1.3
---
+++ python_canteen.py
@@ -0,0 +1,60 @@
+from bottle import run, route, view, get, post, request
+from itertools import count
+
+class Food:
+
+    _ids = count (0)
+
+    def __init__(self,food_name, food_num): 
+        self.id = next(self._ids)
+        self.food_name = food_name
+        self.food_num = food_num
+
+
+foods = [
+    Food("Mini pizza", 3),
+    Food("Sushi roll", 9),
+    Food("Pie", 13)
+    ]
+
+@route('/index')
+@view ('index')
+def index():
+    pass
+
+
+@route('/card')
+@view ('card')
+def card():
+    data = dict (food_info = foods)
+    return data
+
+@route('/purchase_food/<food_id>')
+@view ('purchase_food')
+def buy_food(food_id):
+
+    food_id = int(food_id)
+    found_food = None
+    for food in foods:
+        if food.id == food_id:
+            found_food = food
+    data = dict (food = found_food)
+    found_food.food_num = found_food.food_num - 1
+    return data
+
+
+@route('/re_stock_food/<food_id>')
+@view ('re_stock_food')
+def re_stock_food(food_id):
+
+    food_id = int(food_id)
+    found_food = None
+    for food in foods:
+        if food.id == food_id:
+            found_food = food
+    data = dict (food = found_food)
+    found_food.food_num = found_food.food_num + 1
+    return data
+
+
+run(host='0.0.0.0', port = 8080, reloader=True, debug=True)
