36afa7ec1a2767784ac72624967270ffe04972f7
gommis <47540262+gommis@users.noreply.github.com>
2019-06-14 12:35:28 +1200

    Add files via upload

--- python_canteen.py
+++ python_canteen.py
@@ -4,7 +4,7 @@ from itertools import count
 class Food:
 
     _ids = count (0)
-
+    # A list with all the different things that will be displayed on the cards e.g. foos number and food price ect
     def __init__(self,food_name, food_num, food_price, food_sold): 
         self.id = next(self._ids)
         self.food_name = food_name
@@ -12,25 +12,26 @@ class Food:
         self.food_price = food_price
         self.food_sold = food_sold
 
-
+# list of all the different foods, their price, their quantity and the number of items that have been sold.
 foods = [
     Food("Mini pizza", 3, "$3", 0),
     Food("Sushi roll", 9, "$4", 0),
     Food("Pie", 13, "$3.50", 0)
-    ]
-
+    ] 
+# The home page
 @route('/')
 @view ('index')
 def index():
     pass
 
-
+# The cards that displays the food, price, quantity and quantity sold.
 @route('/card')
 @view ('card')
 def card():
     data = dict (food_info = foods)
     return data
 
+# Purchase food success code
 @route('/purchase_food/<food_id>')
 @view ('purchase_food')
 def buy_food(food_id):
@@ -45,7 +46,7 @@ def buy_food(food_id):
     found_food.food_sold = found_food.food_sold + 1
     return data
 
-
+# Restocking food succession code
 @route('/re_stock_food/<food_id>')
 @view ('re_stock_food')
 def re_stock_food(food_id):
